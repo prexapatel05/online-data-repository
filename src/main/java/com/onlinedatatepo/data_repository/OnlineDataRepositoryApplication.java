fa99946217136ecabbb2568054d7e96b6753a637
prexapatel05 <prexa2512@gmail.com>
2026-02-24 08:19:45 +0530

    Init commit

---
+++ src/main/java/com/onlinedatatepo/data_repository/OnlineDataRepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.onlinedatatepo.data_repository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineDataRepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineDataRepositoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onlinedatatepo/data_repository/OnlineDataRepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlinedatatepo.data_repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineDataRepositoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
